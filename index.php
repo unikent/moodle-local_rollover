7cc230a85cdc4ca815b6f2c4b5c650478de17efc
Jody Kemsley <jwk8@kent.ac.uk>
2012-01-18 15:31:53 +0000

    Initial commit of Rollover plugin, adding less lib
    Adding the initial commit of the Rollover plugin
    Adding the LESS css lib to moodle

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This is a one-line short description of the file
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod
+ * @subpackage newmodule
+ * @copyright  2011 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/lib.php');
+require_once($CFG->dirroot .'/course/lib.php');
+require_once($CFG->libdir .'/filelib.php');
+
+global $USER;
+
+$systemcontext = get_context_instance(CONTEXT_SYSTEM);
+require_login();
+require_capability('moodle/course:update', $systemcontext);
+
+$PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
+$PAGE->set_url('/local/rollover/index.php');
+$PAGE->set_pagelayout('admin'); 
+$PAGE->navbar->add(get_string('pluginname', 'local_rollover'));
+
+$PAGE->set_title(get_string('pluginname', 'local_rollover'));
+$PAGE->set_heading(get_string('pluginname', 'local_rollover'));
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('pluginname', 'local_rollover'));
+
+$scripts ='<link rel="stylesheet/less" type"text/css" href="styles.less">
+    <script src="' . $CFG->wwwroot . '/lib/less/less-1.2.0.min.js" type="text/javascript"></script>';
+
+echo $scripts;
+
+$form = "<div class='rollover_item'>
+            <form>
+                <div class='rollover_crs_title'>
+                    <div class='arrow'></div>
+                    <h3>This is a test title</h3>
+                    <p class='rollover_shrt_code'><span class='rollover_txt_head'>Short code: </span>EL310</p>
+                    <p class='rollover_desc'><span class='rollover_txt_head'>Description: </span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fringilla sem id ligula sagittis ac aliquet eros vehicula. Sed rhoncus sapien ac dui cursus consectetur. Nunc lacinia elementum quam non aliquet. Donec venenatis, odio sed pharetra rutrum, purus dui tristique orci, eu fringilla mauris dui vitae ante</p>
+                </div>
+                <div class='rollover_crs_from'>
+                    <div class='arrow'></div>
+                    <div class='rollover_crs_search'>
+                        <input type='text' class='rollover_crs_input'/>
+                        <button type='buttons' class='rollover_crs_submit'>Go</button>
+                    </div>
+                    <h4 class='rollover_advanced_title'>Advanced options</h4>
+                </div>
+            </form>
+         </div>";
+
+echo $form;
+echo $form;
+echo $form;
+echo $form;
+echo $OUTPUT->footer();
---
+++ lib.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Returns list of courses in suitable format - just for testing.  Remove later.
+ */
+function kent_list_rollover_courses(){
+
+    $courses = kent_get_empty_courses();
+
+    if(!empty($courses)){
+     
+       $output = "<ul>";
+       foreach($courses as $course){
+           $output .= "<li>" . $course->id . " - " . $course->shortname . "</li>";
+       }
+       $output .= "</ul>";
+
+       return $output;
+
+    } else {
+        return "<p>Sorry.  No courses are accessible.</p>";
+    }
+
+
+}
+
+
+/**
+ * Returns list of courses for user
+ */
+function kent_get_empty_courses(){
+    global $USER, $DB;
+
+    $context = get_context_instance(CONTEXT_SYSTEM);
+
+    //TODO - Need to check the rollover log table once its ready as well maybe?
+   
+    //If we are an admin, then we need to see all courses
+    if (has_capability('moodle/site:config', $context)){
+        $courses = kent_get_all_courses("all", "c.sortorder ASC", "c.id, c.shortname");
+    } else {
+        $courses = kent_get_own_editable_courses();
+    }
+
+    return $courses;
+
+}
+
+
+/**
+ * Returns all courses that are empty and user with update course permissions can see
+ */
+function kent_get_own_editable_courses(){
+
+    global $CFG, $USER, $DB;
+
+    //Old SQL - remove later
+        //$sql = "SELECT DISTINCT con.id, con.contextlevel, con.instanceid, ra.userid, ra.contextid, ra.roleid, c.*
+        //        FROM {$CFG->prefix}context con
+        //        JOIN {$CFG->prefix}role_assignments ra ON userid = 3 AND con.id = ra.contextid AND roleid IN (SELECT DISTINCT roleid FROM `mdl_role_capabilities` WHERE `capability` = 'moodle/course:update' AND permission = 1 ORDER BY roleid ASC)
+        //        JOIN mdl_course c ON c.id = con.instanceid
+        //        WHERE con.contextlevel = 50";
+
+    $course_list = array();
+    $params['userid'] = (int)$USER->id;
+    $params['capability'] = 'moodle/course:update';
+
+    $sql = "SELECT DISTINCT c.id, c.sortorder, c.fullname, c.shortname, c.fullname, c.summary, c.visible
+            FROM {$CFG->prefix}context con
+            JOIN {$CFG->prefix}role_assignments ra ON userid=:userid AND con.id=ra.contextid AND roleid IN (SELECT DISTINCT roleid FROM {$CFG->prefix}role_capabilities rc WHERE rc.capability=:capability AND rc.permission=1 ORDER BY rc.roleid ASC)
+            JOIN mdl_course c ON c.id=con.instanceid
+            LEFT JOIN {$CFG->prefix}course_sections cse ON cse.course = c.id AND length(cse.summary)>0 AND cse.section != 0
+            LEFT JOIN {$CFG->prefix}course_modules cms ON c.id = cms.course
+            WHERE cms.course is null AND cse.section is null AND con.contextlevel=50";
+
+
+    
+    // pull out all course matching the cat
+    if ($courses = $DB->get_records_sql($sql, $params)) {
+
+        foreach ($courses as $course) {
+            if ($course->id == 1) continue;
+            $course_list[$course->id] = $course;
+        }
+    }
+
+    return $course_list;
+
+}
+
+
+
+
+
+/**
+ * Returns all empty courses of courses, for whole site, or category.  For admin use only!
+ */
+function kent_get_all_courses($categoryid="all", $sort="c.sortorder ASC", $fields="c.*") {
+
+    global $USER, $CFG, $DB;
+
+    $params = array();
+    $course_list = array();
+
+
+    //FG30 - Updated SELECT to only get empty courses
+//    $sql = "SELECT DISTINCT $fields $ccselect
+//              FROM {course} c
+//           $ccjoin
+//           JOIN {$CFG->prefix}course_sections cse ON cse.course = c.id AND length(cse.summary) = 0 AND cse.section != 0
+//           LEFT JOIN {$CFG->prefix}course_modules cms ON c.id = cms.course WHERE cms.course is null
+//              $categoryselect
+//              $sortstatement";
+
+    $context = get_context_instance(CONTEXT_SYSTEM);
+    //Only return everything if an admin...
+    if (has_capability('moodle/site:config', $context)){
+
+        $sql = "SELECT DISTINCT c.id, c.shortname, c.fullname, c.summary, c.visible, ctx.id AS ctxid, ctx.path AS ctxpath, ctx.depth AS ctxdepth, ctx.contextlevel AS ctxlevel, ctx.instanceid AS ctxinstance, cms.course, cse.section
+                FROM {$CFG->prefix}course c
+                LEFT JOIN {$CFG->prefix}context ctx ON (ctx.instanceid = c.id AND ctx.contextlevel = 50)
+                LEFT JOIN {$CFG->prefix}course_sections cse ON cse.course = c.id AND length(cse.summary)>0 AND cse.section != 0
+                LEFT JOIN {$CFG->prefix}course_modules cms ON c.id = cms.course
+                WHERE cms.course is null AND cse.section is null
+                ORDER BY c.sortorder ASC";
+
+        // pull out all course matching the cat
+        if ($courses = $DB->get_records_sql($sql, $params)) {
+
+            // loop throught them
+            foreach ($courses as $course) {
+                if ($course->id == 1) continue;
+                $course_list[$course->id] = $course;
+            }
+        }
+    }
+
+    return $course_list;
+}
+
+
+
