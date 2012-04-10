197937a98176b5adfc6ec1f9f1059841bca6dfc6
Dave Clark <dec6@kent.ac.uk>
2012-04-10 11:10:03 +0100

    Added auth mechanism for rollover UI
    The UI now makes two requests, one to moodle 1's /auth/saml, to log the
    user in via SSO, and then a second to get the modulelist data.

--- scripts/autoComplete.js
+++ scripts/autoComplete.js
@@ -1,75 +1,98 @@
 jQuery(document).ready(function() {
 
-    var courses_search = new Array();
-    var courses = new Array();
+	var courses_search = new Array();
+	var courses = new Array();
 
-    jQuery.blockUI({ message: '<div class="blockui_loading">Please wait, loading course lists.</div>' });
+	jQuery.blockUI({ message: '<div class="blockui_loading">Please wait, loading course lists.</div>' });
 
-    //Mock url for localhost... remove later
-    //window.autoCompleteUrl = 'http://localhost/moodle-www/kent/modulelist/index.php?action=modlist&terms=co32';
-    //window.autoCompleteUrl = 'http://localhost/moodle/kent/modulelist/index.php?action=allmodlist'
-    
-    $("#dialog_autocomplete_error").dialog({
-         autoOpen: false,
-         title: 'Rollover error',
-         modal: true,
-         buttons: {
-            "OK": function() {
-                $(this).dialog("close");
-            }
-         }
-    });
+	//Mock url for localhost... remove later
+	//window.autoCompleteUrl = 'http://localhost/moodle-www/kent/modulelist/index.php?action=modlist&terms=co32';
+	//window.autoCompleteUrl = 'http://localhost/moodle/kent/modulelist/index.php?action=allmodlist'
 
-    jQuery.ajax({
-      url: window.autoCompleteUrl,
-      dataType: 'json',
-      success: function(data){
+	$("#dialog_autocomplete_error").dialog({
+		 autoOpen: false,
+		 title: 'Rollover error',
+		 modal: true,
+		 buttons: {
+			"OK": function() {
+				$(this).dialog("close");
+			}
+		 }
+	});
 
-        //First check if we have courses and error if so.
-        if (data === null){
-            jQuery.unblockUI();
-            $("#dialog_autocomplete_error").dialog("open");
-        } else {
+	// we're making two requests, one to the saml auth page, to allow SSO to log
+	// the user in if they're currently unauthed, and then a second to get
+	// the actual autocomplete data
 
-            jQuery.unblockUI();
-            if (data !== null){
-                for(var course in data.courses) {
-                    courses_search.push(data.courses[course].fullname);
-                    courses[data.courses[course].fullname] = [course, '1.9'];
-                }
-            }
+	// although one request would be preferable, it's a little flaky with SSO
+	// and there's no guarantee SSO will redirect the user back to the auto-
+	// complete api when they're done.
+	
+	jQuery.ajax({
+		url: window.oldMoodleAuthUrl,
+		success: function(data) {
+			
+			// hopefully the user has been logged in by now, so we can make the
+			// modulelist api request
 
-            jQuery('.rollover_crs_input').autocomplete({
-                minLength: 1,
-                source: function(request, response) {
-                    var results = jQuery.ui.autocomplete.filter(courses_search, request.term);
-                    response(results.slice(0, 30));
-                },
-                delay: 0,
-                select: function(event, ui) {
-                    jQuery(this).closest('.rollover_crs_from').find('.id_from').val(courses[ui.item.label][0]);
-                    if(courses[ui.item.label][1] === '1.9') {
-                        jQuery(this).parent().find('.m1 input').attr('disabled', 'disabled').removeAttr('checked');
-                    } else {
-                        jQuery(this).parent().find('.m1 input').attr('checked', 'checked').removeAttr('disabled');
-                    }
-                },
-                change: function(event, ui){
-                    var change_results = jQuery.ui.autocomplete.filter(courses_search, jQuery(this).val().trim());
-                    if(change_results.length === 0 || jQuery(this).val() === ''){
-                        jQuery(this).closest('.rollover_crs_from').find('.id_from').val('');
-                    } else {
-                        jQuery(this).closest('.rollover_crs_from').find('.id_from').val(courses[change_results[0]][0]);
-                    }
-                }
-            });
-        }
-      },
-      error: function(x, t, m){
-            jQuery.unblockUI();
-            $("#dialog_autocomplete_error").dialog("open");
-      },
-      timeout: 20000 //20 Seconds max to try and fetch
-    });
+			jQuery.ajax({
+				url: window.autoCompleteUrl,
+				dataType: 'json',
+				success: function(data){
+
+					//First check if we have courses and error if so.
+					if (data === null){
+						jQuery.unblockUI();
+						$("#dialog_autocomplete_error").dialog("open");
+					} else {
+
+						jQuery.unblockUI();
+						if (data !== null){
+							for(var course in data.courses) {
+								courses_search.push(data.courses[course].fullname);
+								courses[data.courses[course].fullname] = [course, '1.9'];
+							}
+						}
+
+						jQuery('.rollover_crs_input').autocomplete({
+							minLength: 1,
+							source: function(request, response) {
+								var results = jQuery.ui.autocomplete.filter(courses_search, request.term);
+								response(results.slice(0, 30));
+							},
+							delay: 0,
+							select: function(event, ui) {
+								jQuery(this).closest('.rollover_crs_from').find('.id_from').val(courses[ui.item.label][0]);
+								if(courses[ui.item.label][1] === '1.9') {
+									jQuery(this).parent().find('.m1 input').attr('disabled', 'disabled').removeAttr('checked');
+								} else {
+									jQuery(this).parent().find('.m1 input').attr('checked', 'checked').removeAttr('disabled');
+								}
+							},
+							change: function(event, ui){
+								var change_results = jQuery.ui.autocomplete.filter(courses_search, jQuery(this).val().trim());
+								if(change_results.length === 0 || jQuery(this).val() === ''){
+									jQuery(this).closest('.rollover_crs_from').find('.id_from').val('');
+								} else {
+									jQuery(this).closest('.rollover_crs_from').find('.id_from').val(courses[change_results[0]][0]);
+								}
+							}
+						});
+					}
+				},
+				error: function(x, t, m){
+					jQuery.unblockUI();
+					$("#dialog_autocomplete_error").dialog("open");
+				},
+				timeout: 20000 //20 Seconds max to try and fetch
+			});
+		},
+		error: function(x, t, m) {
+			jQuery.unblockUI();
+			$("#dialog_autocomplete_error").dialog("open");
+		}
+	})
+
+	
 });
 
