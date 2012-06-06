cc2ea8ddbf37674b6c60204b6ae35a1f7bfb54c1
Jody Kemsley <jwk8@kent.ac.uk>
2012-06-06 15:50:53 +0100

    Fixing the rollover so the hide and show only triggers for the clicked item

--- scripts/hideshow.js
+++ scripts/hideshow.js
@@ -3,16 +3,17 @@ jQuery(function($) {
         if(e.target === $('.iconhelp',this)[0]){
             return true;        
         }
-        if($('.rollover_advanced_options').hasClass('open')) {
-            $('.text',this).text('Show advanced options');
-            $('.rollover_advanced_options').stop(true, true).slideUp('fast');
-            $('.rollover_advanced_options').removeClass('open');
+        var _this = $(this).closest('.more_advanced_wrap').siblings('.rollover_advanced_options');
+        if(_this.hasClass('open')) {
+            $('.text', this).text('Show advanced options');
+            $(_this).stop(true, true).slideUp('fast');
+            $(_this).removeClass('open');
             $('.arrow_border', this).show();
             $('.arrow_light', this).show();
         } else {
-            $('.text',this).text('Hide advanced options');
-            $('.rollover_advanced_options').stop(true, true).slideDown('fast');
-            $('.rollover_advanced_options').addClass('open');
+            $('.text', this).text('Hide advanced options');
+            $(_this).stop(true, true).slideDown('fast');
+            $(_this).addClass('open');
             $('.arrow_border', this).hide();
             $('.arrow_light', this).hide();
         }
