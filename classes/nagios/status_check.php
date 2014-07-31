5ddb03734296fe097e420db575bdc954b0e9a56f
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-07-31 11:00:35 +0100

    Switch to nagios plugin

---
+++ classes/nagios/status_check.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_rollover\nagios;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Event Class
+ */
+class status_check extends \local_nagios\base_check
+{
+    /**
+     * Execute the check.
+     */
+    public function execute() {
+    	global $SHAREDB;
+
+        $errored = $SHAREDB->count_records('rollovers', array(
+            'status' => 3
+        ));
+
+        $queued = $SHAREDB->count_records('rollovers', array(
+            'status' => 0
+        ));
+
+        $queued += $SHAREDB->count_records('rollovers', array(
+            'status' => 1
+        ));
+
+        $inprogress = $SHAREDB->count_records('rollovers', array(
+            'status' => 4
+        ));
+
+        $threshold = 5;
+
+        if ($errored > 0) {
+            $this->error($errored . ' failed rollovers.');
+        }
+
+        if ($queued > $threshold) {
+            $this->warning($queued . ' queued rollovers.');
+        }
+
+        if ($inprogress > $threshold) {
+            $this->warning($inprogress . ' in progress rollovers.');
+        }
+    }
+}
--- db/nagios.php
+++ db/nagios.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,24 +14,16 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-
 /**
- * Defines the version.
+ * Local stuff for Moodle Rollover
  *
  * @package    local_rollover
  * @copyright  2014 Skylar Kelty <S.Kelty@kent.ac.uk>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version   = 2014080100;
-$plugin->component = 'local_rollover';
-$plugin->requires = 2014051200;
-$plugin->cron     = 0;
-$plugin->release = '2.2 (Build: 2014080100)';
-$plugin->maturity = MATURITY_STABLE;
-
-$plugin->dependencies = array(
-    'local_nagios' => 2014080100
+$nagios = array(
+    array(
+        'classname' => 'local_rollover\nagios\status_check'
+    )
 );
