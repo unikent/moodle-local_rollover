9e2000f17613c650bb54bb31e1e7f2062aa086f8
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-11-28 11:25:09 +0000

    Update some strings

--- lang/en/local_rollover.php
+++ lang/en/local_rollover.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -32,14 +31,14 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Kent Rollover';
 $string['no_permissions'] = 'Sorry, but you do not currently have permissions to access this page';
-$string['no_courses'] = 'There are currently no modules which you have access to which require content rollover.  Please contact an administrator if you require any assistance.';
+$string['no_courses'] = 'You currently do not have access to any modules requiring content rollover. Please contact an administrator if you require any assistance.';
 $string['processingmessage'] = '<h3>Scheduled for rollover</h3><p>Your request will be completed in the next 24 hours.</p>';
 $string['requestedmessage'] = '<h3>Successfully scheduled</h3><p>Your request will be completed in the next 24 hours.</p>';
 $string['errormessage'] = '<h3>Error!</h3><p>Please contact your FLT if this problem still<br/>remains after an hour.</p>';
 $string['are_you_sure_text'] = 'Are you sure you want to rollover this module?';
 
 $string['rollover_from_error_text'] = 'Please select a valid existing module to rollover from.';
-$string['rollover_to_error_text'] = 'No destination module set.  Please refresh this page and try again.  If this error persists, please contact an administrator.'; //Should never see this, but just incase.
+$string['rollover_to_error_text'] = 'No destination module set. Please refresh this page and try again. If this error persists, please contact an administrator.';
 //$string['rollover_autocomplete_error'] = 'Could not retrieve autocomplete data.  Please refresh this page and try again.  If this error persists, please contact an administrator.';
 $string['rollover_autocomplete_error'] = 'Could not retrieve autocomplete data. <br/><br/>Please note that you need to be also logged into the current <a target="_blank" href="https://moodle.kent.ac.uk/moodle">Moodle website</a> to carry out rollovers and avoid this error.  This is a temporary measure until student exams are finished and so not to cause interruption to that service.<br/><br/>We apologise for any temporary inconvenience caused.';
 
@@ -47,7 +46,7 @@ $string['rollover_autocomplete_error'] = 'Could not retrieve autocomplete data. 
 $string['short_code_label_text'] = 'Short code';
 $string['description_label_text'] = 'Description';
 
-$string['search_placeholder'] = 'Please enter module name..';
+$string['search_placeholder'] = 'Please enter a module code..';
 $string['advanced_options_label'] = 'Advanced options';
 $string['rollover_button_text'] = 'Rollover';
 $string['no_course_description_text'] = 'No description at this time.';
@@ -55,4 +54,4 @@ $string['no_course_description_text'] = 'No description at this time.';
 //Help text
 $string['top_page_help'] = '<p>To rollover content from a previous module to a module listed below, please select a module to rollover from and click the rollover button.</p>';
 $string['advanced_opt_help'] = 'Advanced rollover options';
-$string['advanced_opt_help_help'] = 'By default, all module components are rolled over.  Uncheck any components you do not wish to have as part of your rollover.';
+$string['advanced_opt_help_help'] = 'By default, all types of module content are rolled over. Uncheck any content you do not wish to have as part of your rollover.';
