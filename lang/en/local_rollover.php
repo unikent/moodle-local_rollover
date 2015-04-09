cf1b348a2f6da67721a08577780fd6360a7754c2
Skylar Kelty <s.kelty@kent.ac.uk>
2015-04-09 12:00:49 +0100

    Remove old langs

--- lang/en/local_rollover.php
+++ lang/en/local_rollover.php
@@ -32,22 +32,8 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Kent Rollover';
 $string['no_permissions'] = 'Sorry, but you do not currently have permissions to access this page';
 
-// @deprecated
-$string['no_courses'] = 'You currently do not have access to any modules requiring content rollover. Please contact an administrator if you require any assistance.';
-$string['processingmessage'] = '<h3>Scheduled for rollover</h3><p>Your request will be completed in the next 24 hours.</p>';
 $string['requestedmessage'] = '<h3>Successfully scheduled</h3><p>Your request will be completed in the next 24 hours.</p>';
 $string['errormessage'] = '<h3>Error!</h3><p>Please contact your FLT if this problem still<br/>remains after an hour.</p>';
-$string['rollover_button_text'] = 'Rollover';
-$string['top_page_help'] = '<p>To rollover content from a previous module to a module listed below, please select a module to rollover from and click the rollover button.</p>';
-$string['are_you_sure_text'] = 'Are you sure you want to rollover this module?';
-$string['rollover_from_error_text'] = 'Please select a valid existing module to rollover from.';
-$string['rollover_to_error_text'] = 'No destination module set. Please refresh this page and try again. If this error persists, please contact an administrator.';
-//$string['rollover_autocomplete_error'] = 'Could not retrieve autocomplete data.  Please refresh this page and try again.  If this error persists, please contact an administrator.';
-$string['rollover_autocomplete_error'] = 'Could not retrieve autocomplete data. <br/><br/>Please note that you need to be also logged into the current <a target="_blank" href="https://moodle.kent.ac.uk/moodle">Moodle website</a> to carry out rollovers and avoid this error.  This is a temporary measure until student exams are finished and so not to cause interruption to that service.<br/><br/>We apologise for any temporary inconvenience caused.';
-$string['short_code_label_text'] = 'Short code';
-$string['description_label_text'] = 'Description';
-$string['no_course_description_text'] = 'No description at this time.';
-
 
 $string['search_placeholder'] = 'Please enter a module code..';
 $string['advanced_options_label'] = 'Advanced options';
